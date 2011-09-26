91b2203e562974dbebe81ce2a8a39be9887272e8
Alex Young <alex@helicoid.net>
2011-09-26 18:47:57 +0100

    Updated schema

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: node web.js
-schema: node schema.js
--- schema.js
+++ schema.js
@@ -1,7 +1,9 @@
 var pg = require('pg').native
   , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
-  , client;
+  , client
+  , query;
 
 client = new pg.Client(connectionString);
 client.connect();
-client.query('CREATE TABLE visits (date date)');
+query = client.query('CREATE TABLE visits (date date)');
+query.on('end', function() { client.end(); });
