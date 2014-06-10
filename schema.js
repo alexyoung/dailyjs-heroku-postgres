7f0273c8b942ff121ffedf8381127638c9d14238
stanzheng <dev.stanzheng@gmail.com>
2014-06-10 10:08:30 -0400

    can i use native?

--- schema.js
+++ schema.js
@@ -1,4 +1,4 @@
-var pg = require('pg')
+var pg = require('pg').native
   , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
   , client
   , query;
--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express')
   , app = express.createServer(express.logger())
-  , pg = require('pg')
+  , pg = require('pg').native
   , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
   , start = new Date()
   , port = process.env.PORT || 3000
